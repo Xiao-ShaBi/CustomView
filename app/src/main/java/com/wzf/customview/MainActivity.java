3a3bf96b5d8d9d6ede7d126b01e0c724c67b7f9f
wuzhifeng <wuzhifeng@youloft.com>
2017-05-03 13:48:43 +0800

    SwitchView一个仿照ios的开关

--- app/src/main/java/com/wzf/customview/MainActivity.java
+++ app/src/main/java/com/wzf/customview/MainActivity.java
@@ -1,13 +1,30 @@
 package com.wzf.customview;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import com.wzf.customview.simpledemo.ToolsActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.custom_tools)
+    Button customTools;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.custom_tools)
+    public void onViewClicked() {
+        startActivity(new Intent(this, ToolsActivity.class));
     }
 }
